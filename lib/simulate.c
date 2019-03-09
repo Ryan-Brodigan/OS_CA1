d900b9c9b69d03d2d015251820abcc8f32ea2003
Ryan-Brdoigan <brianbrodigan@hotmail.com>
2019-03-09 23:51:24 +0000

    Added function that calculates the address offset for a given address

--- lib/simulate.c
+++ lib/simulate.c
@@ -6,33 +6,47 @@
 #define PAGE_SIZE_BYTES 256
 
 void dumpPhysicalMemoryToDisk(unsigned short *physical_memory);
+unsigned short calculateAddressOffset(unsigned short address);
 
 void simulate(){
-	// Allocate a chunk of physical memory that covers the address space for our 16-Bit System
+	// Allocate a chunk of memory that represents the physical memory of our system
 	unsigned short *physical_memory = malloc(MAX_ADDRESSABLE_BYTES * sizeof(short));
+	//Allocate a chunk of memory that represents the virtual address space of our process
+	unsigned short *virtual_memory = malloc(MAX_ADDRESSABLE_BYTES * sizeof(short));
+	//Allocate a chunk of memory that represents the page table for our system
+	unsigned short *page_table = malloc(PAGE_SIZE_BYTES * sizeof(short));
 
-	dumpPhysicalMemoryToDisk(physical_memory);
+	unsigned short test = 257;
+
+	//dumpPhysicalMemoryToDisk(physical_memory);
+	unsigned short result = calculateAddressOffset(test);
 
 	// When we are finished, clean up
 	free(physical_memory);
+	free(virtual_memory);
+	free(page_table);
 }
 
 void dumpPhysicalMemoryToDisk(unsigned short *physical_memory){
 	FILE * physical_memory_dump_file;
-	physical_memory_dump_file = fopen("~/CA1/OS_CA1/data/physical_memory_dump.txt", "w");
+	physical_memory_dump_file = fopen("../data/physical_memory_dump.txt", "w");
 
 	if(physical_memory_dump_file == NULL){
-		printf("ERROR IN FILE");
+		printf("ERROR LOADING PHYSICAL MEMORY DUMP FILE\nExiting...\n");
 		exit(1);
 	}
 
-	fprintf(physical_memory_dump_file, "ADDRESS	|	FRAME	|	CONTENT\n");
-	fprintf(physical_memory_dump_file, "----------------------------------------------\n");
+	fprintf(physical_memory_dump_file, "	ADDRESS	|	FRAME	|	CONTENT\n");
+	fprintf(physical_memory_dump_file, "-----------------------------\n");
 
 	for(int i = 0; i < MAX_ADDRESSABLE_BYTES; i++){
-		fprintf(physical_memory_dump_file, "0x%X	|	%d	|	00\n", i, i >> 8);
+		fprintf(physical_memory_dump_file, "	0x%X	|		%d	|	00\n", i, i >> 8);
 	}
 
 	fclose(physical_memory_dump_file);
 }
 
+unsigned short calculateAddressOffset(unsigned short address){
+	unsigned short addressStart = (address >> 8) * PAGE_SIZE_BYTES;
+	return (address - addressStart);
+}
