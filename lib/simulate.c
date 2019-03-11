561a25dd9d1a5d160e9cdd8c843b90106508355e
Ryan-Brdoigan <brianbrodigan@hotmail.com>
2019-03-11 00:48:20 +0000

    Added a method that randomly writes data (in the form of characters) to our physical_memory

--- lib/simulate.c
+++ lib/simulate.c
@@ -7,6 +7,8 @@
 
 void dumpPhysicalMemoryToDisk(unsigned short *physical_memory);
 unsigned short calculateAddressOffset(unsigned short address);
+void randomWriteToPhysicalMemory(unsigned short *physical_memory);
+int randomIntInRange(int min, int max);
 
 void simulate(){
 	// Allocate a chunk of memory that represents the physical memory of our system
@@ -16,10 +18,9 @@ void simulate(){
 	//Allocate a chunk of memory that represents the page table for our system
 	unsigned short *page_table = malloc(PAGE_SIZE_BYTES * sizeof(short));
 
-	unsigned short test = 257;
-
 	//dumpPhysicalMemoryToDisk(physical_memory);
-	unsigned short result = calculateAddressOffset(test);
+	//unsigned short result = calculateAddressOffset(test);
+	//randomWriteToPhysicalMemory(physical_memory);
 
 	// When we are finished, clean up
 	free(physical_memory);
@@ -49,4 +50,29 @@ void dumpPhysicalMemoryToDisk(unsigned short *physical_memory){
 unsigned short calculateAddressOffset(unsigned short address){
 	unsigned short addressStart = (address >> 8) * PAGE_SIZE_BYTES;
 	return (address - addressStart);
+}
+
+void randomWriteToPhysicalMemory(unsigned short *physical_memory){
+	//Generate random number between 2048-20480, the bytes of data we will write to our memory
+	int randomBytes = randomIntInRange(2048, 20480);
+
+	//Randomly write the aforementioned amount of data to our memory, using a loop
+	//We will randomly write characters, which are 1 byte each, so loop for the amount of
+	//bytes we randomly generated
+	for(int i = 0; i < randomBytes; i++){
+	
+		//Randomly pick an address in our physical memory to write to
+		int randomAddress = randomIntInRange(0, (MAX_ADDRESSABLE_BYTES - 1));
+
+		//Randomly generate a character to write to the aforementioned address
+		char randomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[random () % 26];
+
+		//Assign the character to our physical memory address
+		physical_memory[i] = &randomChar;
+		
+	}
+}
+
+int randomIntInRange(int min, int max){
+	return (min + rand() / (RAND_MAX / (max - min + 1) + 1));
 }
